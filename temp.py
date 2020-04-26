4ffdeef4516f24c172c204a0e367c954a2408fd7
blt <52168338+amyhsiao@users.noreply.github.com>
2020-04-26 21:09:34 +0800

    removed grade

--- temp.py
+++ temp.py
@@ -1,9 +1,9 @@
 grade = {
-"math" : "78.8",
-"history" : "88",
-"geo" : "84.5",
-"chinese" : "73",
-"english" : "93",
-"chem" : "90.1",
-"bio" : "92.4",
+"math" : "-1",
+"history" : "-1",
+"geo" : "-1",
+"chinese" : "-1",
+"english" : "-1",
+"chem" : "-1",
+"bio" : "-1",
 }
